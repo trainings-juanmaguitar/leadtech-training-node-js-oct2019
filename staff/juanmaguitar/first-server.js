6f3e089c95b1709a1ad2fea87e698c3b60d71bb0
JuanMa Garrido <juanma.garrido@gmail.com>
2019-10-29 09:38:44 +0100

    session1 examples

---
+++ staff/juanmaguitar/first-server.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const hostname = '127.0.0.1'
+const port = process.env.PORT
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 201
+  res.setHeader('Content-Type', 'text/plain')
+  res.setHeader('Company-From', 'leadtech')
+  res.end('Hello World!\n')
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
